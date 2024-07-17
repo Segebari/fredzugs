fd64dae141c6cb05a7dc6b39db8b8e4155e1f6ac
Segebari <segebarifred@gmail.com>
2024-07-17 19:02:33 +0100

    ccc

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,24 +1,18 @@
-import React from "react";
+import React from 'react'
 import img from "../../assets/img/fred.png";
 
 const Hero = () => {
   return (
     <>
-      <div className="relative flex flex-col md:flex-row w-full h-screen overflow-hidden">
-        <div className="w-full md:w-[15%]"></div>
-        <div className="flex-grow flex justify-center items-end">
-          <img
-            src={img}
-            alt="Me"
-            className="w-auto max-w-full h-auto max-h-[85vh] object-contain"
-          />
-        </div>
+      <div className="flex w-full h-[90vh] overflow-y-hidden">
+        <div className="w-[15%]"></div>
+        <img src={img} alt="Me" className=" h-[30rem] object-cover md:object-contain" />
         <div className="hidden md:flex w-[40%] items-center justify-left text-3xl">
           Front-End & React Native <br /> Developer
         </div>
       </div>
 
-      <div className="relative mt-[-6rem] md:mt-[-8rem] text-6xl md:text-9xl overflow-hidden flex flex-nowrap">
+      <div className="relative mt-[-8rem] text-9xl overflow-hidden flex flex-nowrap">
         <span className="inline-block mr-8 animate-marquee whitespace-nowrap">
           Fred Zugs -
         </span>
@@ -37,6 +31,6 @@ const Hero = () => {
       </div>
     </>
   );
-};
+}
 
-export default Hero;
+export default Hero
