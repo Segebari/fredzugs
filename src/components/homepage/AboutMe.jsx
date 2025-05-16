bc9b6c3713dba4b33c485d453e6be64999f49e37
Fred Zugs <segebarifred@gmail.com>
2025-05-16 08:45:16 +0100

    About me edit

--- src/components/homepage/AboutMe.jsx
+++ src/components/homepage/AboutMe.jsx
@@ -31,7 +31,7 @@ const AboutMe = () => {
                     text-base sm:text-lg md:text-xl lg:text-2xl 
                     text-justify sm:text-left"
       >
-        I'm Fred Zugs, a Full-Stack Developer specializing in the MERN stack and a versatile range of technologies. With a design-centric background from advertising, I'm passionate about building visually appealing and intuitive web and mobile applications. My proficiency spans React, React Native, Tailwind CSS, Next.js, Node.js, Express, Angular, Vue, Python, AWS, Azure, Prisma, Redis, PostgreSQL (with pgAdmin4), SQL, MongoDB, Docker, and Kubernetes. Bringing over three years of tech experience, I'm dedicated to delivering seamless, creative, and robust full-stack solutions. I'm committed to continuous growth, collaborative knowledge sharing, and solving complex problems through well-crafted code.
+        I'm Fred Zugs, a Full-Stack Developer specializing in the MERN stack and a versatile range of technologies. With a design-centric background from advertising, I'm passionate about building visually appealing and intuitive web and mobile applications. My proficiency spans React, React Native (Expo), Tailwind CSS, Next.js, Node.js, Express, Angular, Vue, Python, AWS, Azure, Prisma, Redis, PostgreSQL (with pgAdmin4), SQL, MongoDB, Docker, and Kubernetes. Bringing over three years of tech experience, I'm dedicated to delivering seamless, creative, and robust full-stack solutions. I'm committed to continuous growth, collaborative knowledge sharing, and solving complex problems through well-crafted code.
       </p>
     </div>
   );
